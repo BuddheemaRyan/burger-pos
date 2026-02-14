26df6e69ad669a5b7a5b5521cb95bb9967a6b793
BuddheemaRyan <buddimaraya16@gmail.com>
2026-02-14 22:55:57 +0530

    completed product.service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface ProductDto{
+  id: number | null;
+  name: string;
+  category: string;
+  price: number;
+  imageUrl?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private apiUrl = 'http://localhost:8080/product';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<ProductDto[]>{
+    return this.http.get<ProductDto[]>(this.apiUrl);
+  }
+
+  create(product: ProductDto): Observable<ProductDto>{
+    return this.http.post<ProductDto>(this.apiUrl,product);
+  }
 }
